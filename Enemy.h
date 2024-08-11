b2c798af0be93bdaf3b59005ca2881cda775f1d7
kitajima-hayato <k023g0057@g.neec.ac.jp>
2024-08-11 16:22:07 +0900

    敵の状態遷移を実装

--- Enemy.h
+++ Enemy.h
@@ -4,6 +4,11 @@
 #include <ViewProjection.h>
 #include <WorldTransform.h>
 #include <cassert>
+// 行動フェーズ
+enum class Phase {
+	Approach, // 接近する
+	Leave,    // 離脱する
+};
 class Enemy {
 
 public:
@@ -11,8 +16,18 @@ public:
 	void Update();
 	void Draw(const ViewProjection& viewProjection);
 
+	//エネミーの接近
+	void Access();
+	//エネミーの離脱
+	void Withdrawal();
+
 private:
 	WorldTransform worldTransform_;
-	Model* model_;
+	Model* model_ = nullptr;
 	uint32_t textureHandle_ = 0u;
+	//エネミーの移動速度
+	const float kSpeed_ = 0.1f;
+	Vector3 velocity = {0, 0, kSpeed_};
+	// フェーズ
+	Phase phase_ = Phase::Approach;
 };
